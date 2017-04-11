358f98d1b1136603f3d75e4ab4ea17571c56f024
Charles Peden and Sean Peterson <charles@>
2017-04-11 09:50:35 -0700

    add authentication, muck about with navbar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NewDeckComponent } from './new-deck/new-deck.component';
 import { DeckManagerComponent } from './deck-manager/deck-manager.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const appRoutes: Routes = [
   {
@@ -13,6 +14,10 @@ const appRoutes: Routes = [
   {
     path: 'new-deck',
     component: NewDeckComponent
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
   },
   {
     path: 'decks/:id',
---
+++ src/app/providers/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+
+@Injectable()
+export class AuthService {
+  currentUserName: string;
+  currentUserId: string;
+
+  constructor(public af: AngularFire) { }
+
+  loginWithGoogle() {
+    return this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    });
+  }
+
+  logout() {
+    this.currentUserName = null;
+    return this.af.auth.logout();
+  }
+
+
+}
