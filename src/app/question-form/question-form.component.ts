852328b21dd734b59be046a1e4271f6855472d25
Charles Peden and Sean Peterson <charles@>
2017-04-11 11:51:40 -0700

    add authorId from service

--- src/app/question-form/question-form.component.ts
+++ src/app/question-form/question-form.component.ts
@@ -1,25 +1,43 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Deck } from './../deck.model';
+import { DeckService } from './../deck.service';
 import { Answer } from './../answer.model';
 import { Question } from './../question.model';
 import { QuestionService } from './../question.service';
+import { AuthService } from './../providers/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-question-form',
   templateUrl: './question-form.component.html',
   styleUrls: ['./question-form.component.css'],
-  providers: [ QuestionService ]
+  providers: [ QuestionService, AuthService, DeckService ]
 })
 export class QuestionFormComponent implements OnInit {
   @Input() deckId: string;
   question: Question;
   answered: boolean = false;
+  deckAuthor: string;
+  currentDeck: Deck;
 
-  constructor(private questionService: QuestionService) { }
+  constructor(private questionService: QuestionService, private authService: AuthService, private router: Router, private deckService: DeckService) {  }
 
   ngOnInit() {
+    this.authService.af.auth.subscribe(
+      (auth) => {
+        if (auth == null) {
+          this.router.navigate(['login']);
+          this.deckAuthor = null;
+          this.authService.currentUserId = null;
+        } else {
+          this.deckAuthor = auth.google.displayName;
+          this.authService.currentUserId = auth.uid;
+        }
+      }
+    );
     this.question = new Question("", "", [], []);
   }
+
 
   appendTag(tagToAppend: string) {
     if (!this.question.tags.includes(tagToAppend)) {
@@ -74,9 +92,11 @@ export class QuestionFormComponent implements OnInit {
   }
 
   submitForm(question: Question) {
-    question.deck = this.deckId;
-    this.questionService.saveQuestion(question);
-    this.question = new Question("", "", [], []);
+
+      question.deck = this.deckId;
+      question.authorId = this.deckAuthor;
+      this.questionService.saveQuestion(question);
+      this.question = new Question("", "", [], []);
   }
 
 }
